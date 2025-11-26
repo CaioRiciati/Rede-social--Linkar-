7cf1d53bc2d5939a62eeb7183473149e914f572d
Ana <anaclarame21@gmail.com>
2025-11-26 08:24:02 -0300

    tarefa

--- project/src/main/java/com/linkar/project/model/Tarefa.java
+++ project/src/main/java/com/linkar/project/model/Tarefa.java
@@ -1,5 +1,48 @@
 package com.linkar.project.model;
 
+import java.time.LocalDate;
+
+import com.linkar.project.inum.Prioridade;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tarefa")
 public class Tarefa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(name = "titulo_tarefa")
+    private String titulo;
+
+    private String descricao;
+
+    private LocalDate data;
+
+    @Enumerated(EnumType.STRING)
+    private Prioridade prioridade;
+
+    // getters e setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(Long idUsuario) { this.idUsuario = idUsuario; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public LocalDate getData() { return data; }
+    public void setData(LocalDate data) { this.data = data; }
+
+    public Prioridade getPrioridade() { return prioridade; }
+    public void setPrioridade(Prioridade prioridade) { this.prioridade = prioridade; }
 }
