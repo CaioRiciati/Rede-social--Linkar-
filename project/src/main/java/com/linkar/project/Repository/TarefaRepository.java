90c6f9ba8a75a602931ec7c024a71cc546b2466f
Ana <anaclarame21@gmail.com>
2025-11-26 08:24:32 -0300

    tarefa

--- project/src/main/java/com/linkar/project/Repository/TarefaRepository.java
+++ project/src/main/java/com/linkar/project/Repository/TarefaRepository.java
@@ -1,5 +1,13 @@
 package com.linkar.project.Repository;
 
-public class TarefaRepository {
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.linkar.project.model.Tarefa;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    List<Tarefa> findByIdUsuarioAndDataOrderByPrioridadeAsc(Long idUsuario, LocalDate data);
 }
